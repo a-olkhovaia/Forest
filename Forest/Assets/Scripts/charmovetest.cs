d7a7ec3ac5705ae03a7f94c29491729ee5d58aec
thenotoriousgrant <147095148+thenotoriousgrant@users.noreply.github.com>
2024-11-18 13:49:00 +0300

    update jump

--- Forest/Assets/Scripts/charmovetest.cs
+++ Forest/Assets/Scripts/charmovetest.cs
@@ -12,45 +12,60 @@ public class PF : MonoBehaviour
     public Transform feetPos;
 
     public float speed;
-    public float jumpheight;
-
+    public float jumpHeight;
 
     public bool moving;
     private Animator anim;
     public bool istalking;
     private float movspeed;
 
-
+    private int jumpCount;         
+    public int maxJumps = 2;       
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
+
     void Update()
     {
+        
         isGrounded = Physics2D.OverlapCircle(feetPos.position, CheckRadius, whatIsGround);
 
-        if (istalking) movspeed = 0; else movspeed = Input.GetAxis("Horizontal");
+        
+        if (isGrounded)
+        {
+            jumpCount = 0;
+        }
+
+        
+        if (istalking)
+        {
+            movspeed = 0;
+        }
+        else
+        {
+            movspeed = Input.GetAxis("Horizontal");
+        }
 
         Flip();
 
-
         rb.velocity = new Vector2(movspeed * speed, rb.velocity.y);
 
-
-
-        if (Input.GetKeyDown(KeyCode.Space) && !istalking)
+        
+        if (Input.GetKeyDown(KeyCode.Space) && !istalking && jumpCount < maxJumps)
         {
-            rb.AddForce(transform.up * jumpheight, ForceMode2D.Impulse);
-            moving = rb.velocity.x != 0 ? true : false;
-
-
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight); 
+            jumpCount++; 
         }
+
+        
         moving = rb.velocity.x != 0 ? true : false;
         anim.SetBool("ismoving", moving);
         anim.SetBool("IsJumping", !isGrounded);
     }
+
     void Flip()
     {
         if (movspeed > 0)
@@ -58,6 +73,7 @@ public class PF : MonoBehaviour
         if (movspeed < 0)
             transform.localRotation = Quaternion.Euler(0, 180, 0);
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name.Equals("Platform"))
@@ -65,6 +81,7 @@ public class PF : MonoBehaviour
             this.transform.parent = collision.transform;
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         transform.parent = null;
