c1ae322b0e5ac59c83bd33bb685b2899e95f82ca
Щукин Владимир <RusiSLin@gmail.com>
2025-04-25 21:21:38 +0300

    changes for easier testing

--- Forest/Assets/health.cs
+++ Forest/Assets/health.cs
@@ -5,18 +5,20 @@ using UnityEngine.Rendering.UI;
 
 public class health : MonoBehaviour
 {
+    public float maxhp;
     public float hp;
     GameObject hpbr;
     // Start is called before the first frame update
     void Start()
     {
-        hp = 5;
+        hp = maxhp;
         hpbr = GameObject.FindGameObjectWithTag("hpbar");
     }
 
     // Update is called once per frame
     void Update()
     {
-        hpbr.transform.localScale = new Vector2((hp / 5), 0.05f);
+        hpbr.transform.localScale = new Vector2((hp / maxhp), 0.05f);
+        if (hp <= 0) hp = 0;
     }
 }
